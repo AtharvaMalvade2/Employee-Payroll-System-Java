76c47010ce30201a1a8e4c020888dbabc88f7203
AtharvaMalvade2 <arth3134@gmail.com>
2025-07-04 17:05:01 +0530

    create abstract class Employee, add getter setters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
+    abstract class Employee {
+        private String name;
+        private int id;
+
+        public Employee(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getId() {
+            return id;
+        }
+    }
+        public static void main(String[] args) {
 
     }
 }
