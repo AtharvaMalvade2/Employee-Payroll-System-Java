ff0bada89afa2849754f94ef28e1d575abd0c1c4
AtharvaMalvade2 <arth3134@gmail.com>
2025-07-04 17:21:30 +0530

    create  FullTime Employee class extending Employee and implementing calculateSalary()

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+
     abstract class Employee {
         private String name;
         private int id;
@@ -24,7 +24,23 @@ public class Main {
             return "Employee [name=" + name + ", id=" + id + ", salary=" + calculateSalary() + "]";
         }
     }
-        public static void main(String[] args) {
 
+
+class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
     }
 }
+
+ public class Main {
+        public static void main(String[] args) {
+
+        }
+    }
